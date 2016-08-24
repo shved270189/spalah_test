a5254d3ea5d2357915f0f68b932dd80684d34eec
Ivan Bondarenko <bondarenko.dev@gmail.com>
2016-08-24 20:08:38 +0300

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,9 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+  # Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: 'Ivan',
+            email: 'spalah_test@gmail.com',
+            password: '123456',
+            password_confirmation: '123456')
